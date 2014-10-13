90775562a8b7e88a40489df6b869646e5cf34404
feilste <filip.stenbeck@kambi.com>
2014-10-13 09:50:41 +0200

    clenup

--- frontend/test/build/specs.js
+++ frontend/test/build/specs.js
@@ -23067,10 +23067,7 @@ require('angular').module('classified').factory('adService', function ($http) {
    var ROOT_URL = 'http://localhost:9000/ad';
    
    var makeGetRequest = function(url, callback) {
-    	$http({
-            method : 'GET',
-            url : url,
-        }).success(function (data) {
+    	$http.get(url).success(function (data) {
             callback(data);
         }).error(function(data, status, headers, config) {
         	console.log("Error getting data status: ", status);
